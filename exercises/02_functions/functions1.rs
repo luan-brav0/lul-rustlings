188e6f0d1f0d474bed8cca6d9b0f4d4266ee4674
luan-brav0 <luan-brav0@github.com>
2023-11-27 14:37:50 -0300

    quiz one done! 🎊

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,7 +3,9 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+fn call_me() {
+    println!("Hey, did you call a fn?");
+}
 
 fn main() {
     call_me();
