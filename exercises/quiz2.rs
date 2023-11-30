5fb5468d7e83181867763a9f56455a77f6d54528
luan-brav0 <luan-brav0@github.com>
2023-11-30 13:53:43 -0300

    Quiz 2 done!

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,21 +32,28 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(&str, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(n) => {
+                    output.push(string.to_owned().to_owned() + "bar".repeat(*n).as_str())
+                }
+            };
         }
-        output
+        return output;
     }
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
